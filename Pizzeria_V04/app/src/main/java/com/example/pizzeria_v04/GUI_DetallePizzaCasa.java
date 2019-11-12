1cdcf07ec70ca07044367d23a573124f82115c8a
rpf1980 <rafapolo1980@gmail.com>
2019-11-12 20:54:49 +0100

    chore(msi): add listener got to GUI_PizzaEnCamino

--- Pizzeria_V04/app/src/main/java/com/example/pizzeria_v04/GUI_DetallePizzaCasa.java
+++ Pizzeria_V04/app/src/main/java/com/example/pizzeria_v04/GUI_DetallePizzaCasa.java
@@ -2,7 +2,10 @@ package com.example.pizzeria_v04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -11,6 +14,7 @@ import java.util.ArrayList;
 public class GUI_DetallePizzaCasa extends AppCompatActivity
 {
     ImageView img;
+    Button btnDetallePizzaCasa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +23,17 @@ public class GUI_DetallePizzaCasa extends AppCompatActivity
         setContentView(R.layout.activity_gui__detalle_pizza_casa);
 
         img = findViewById(R.id.idFotoDetalleCasa);
+        btnDetallePizzaCasa = findViewById(R.id.idBtnDetalleCasa);
+
+        btnDetallePizzaCasa.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(getApplicationContext(), GUI_PizzaEnCamino.class);
+                startActivity(i);
+            }
+        });
 
         ArrayList<Datos> arrayItems = new ArrayList<Datos>();
 
--- Pizzeria_V04/app/src/main/java/com/example/pizzeria_v04/GUI_PizzaAlGusto.java
+++ Pizzeria_V04/app/src/main/java/com/example/pizzeria_v04/GUI_PizzaAlGusto.java
@@ -2,6 +2,7 @@ package com.example.pizzeria_v04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,7 +45,8 @@ public class GUI_PizzaAlGusto extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                GetToast.getInstance("Click en Listo !!!");
+                Intent i = new Intent(getApplicationContext(), GUI_DetallePizzaGusto.class);
+                startActivity(i);
             }
         });
 
