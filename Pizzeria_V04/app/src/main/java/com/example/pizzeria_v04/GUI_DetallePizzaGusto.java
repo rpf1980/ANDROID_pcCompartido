3e080edb6a71d92da84d6c62b3887e88b4ce5ae6
rpf1980 <rafapolo1980@gmail.com>
2019-11-12 20:43:47 +0100

    chore(msi): delete singleton GetToast

--- Pizzeria_V04/app/src/main/java/com/example/pizzeria_v04/GUI_DetallePizzaGusto.java
+++ Pizzeria_V04/app/src/main/java/com/example/pizzeria_v04/GUI_DetallePizzaGusto.java
@@ -2,13 +2,39 @@ package com.example.pizzeria_v04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class GUI_DetallePizzaGusto extends AppCompatActivity {
+public class GUI_DetallePizzaGusto extends AppCompatActivity
+{
+    Button btnDetPizGusto;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gui__detalle_pizza_gusto);
+
+        btnDetPizGusto = findViewById(R.id.idBtnDetallesPizzaGusto);
+
+        btnDetPizGusto.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                getToast("Ok click");
+
+                Intent i = new Intent(getApplicationContext(), GUI_PizzaEnCamino.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void getToast(String msg)
+    {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
