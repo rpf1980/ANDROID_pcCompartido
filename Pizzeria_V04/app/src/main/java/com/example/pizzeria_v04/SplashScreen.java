ecf877a57ecd1009f3e72a44fd2b99d4fb4937e3
rpf1980 <rafapolo1980@gmail.com>
2019-11-13 18:22:22 +0100

    feat(msi): add SplashScreen succefully

---
+++ Pizzeria_V04/app/src/main/java/com/example/pizzeria_v04/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.pizzeria_v04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // https://www.youtube.com/watch?v=0cnOTSRCaW4
+        // Usamos un Handler para controlar el tiempo que tarda en verse esta
+        // actividad. Después del tiempo indicado pasará a la actividad siguiente
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        }, 4000);
+    }
+}
